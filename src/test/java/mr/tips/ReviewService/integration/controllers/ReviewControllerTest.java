09d94910525fb71abbf473839f721622574f9445
artem samohvalov <artym.samohvaloff@gmail.com>
2025-05-02 20:18:29 +0400

    test fix

--- src/test/java/mr/tips/ReviewService/integration/controllers/ReviewControllerTest.java
+++ src/test/java/mr/tips/ReviewService/integration/controllers/ReviewControllerTest.java
@@ -2,27 +2,24 @@ package mr.tips.ReviewService.integration.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
-import mr.tips.ReviewService.config.SecurityConfig;
-import mr.tips.ReviewService.controllers.ReviewController;
+import jakarta.transaction.Transactional;
 import mr.tips.ReviewService.dto.CreateReviewRequest;
 import mr.tips.ReviewService.dto.UserImage;
-import mr.tips.ReviewService.filters.JwtAuthenticationFilter;
 import mr.tips.ReviewService.integration.utils.TestJwtUtils;
-import mr.tips.ReviewService.services.ReviewService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ReviewController.class)
-@Import({SecurityConfig.class, TestJwtUtils.class})
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class ReviewControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -30,11 +27,6 @@ public class ReviewControllerTest {
     private ObjectMapper objectMapper;
     @Autowired
     private TestJwtUtils testJwtUtils;
-
-    @MockitoBean
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
-    @MockitoBean
-    private ReviewService reviewService;
 
     @Test
     void createReviewTest() throws Exception {
